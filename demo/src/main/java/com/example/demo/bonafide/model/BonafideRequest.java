2e17d10d57c498b3de07aeda2e9bd6f07ce4b481
Karthick subramanian <karthicksubbiramaniyan@gmail.com>
2024-10-21 07:32:01 +0530

    Create BonafideRequest.java

---
+++ demo/src/main/java/com/example/demo/bonafide/model/BonafideRequest.java
@@ -0,0 +1,24 @@
+package com.example.bonafide.model;
+
+public class BonafideRequest {
+
+    private String date;
+    private String name;
+    private String registerNumber;
+    private String department;
+    private String gender;
+    private String yearSemester;
+    private String guardianName;
+    private String mobileNumber;
+    private String email;
+    private String reason;
+    private String hscMode;
+    private String scholarshipMode;
+    private String diplomaCgpa;
+    private String ugPgCgpa;
+    private String bonafideReceived;
+    private String bonafideReason;
+    private String cgpa;
+
+    // Getters and Setters for all fields
+}
