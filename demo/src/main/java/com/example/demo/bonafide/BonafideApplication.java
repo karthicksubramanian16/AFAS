cad8e182bc3afd3407b1895e483a519bd7c3210b
Karthick subramanian <karthicksubbiramaniyan@gmail.com>
2024-10-21 07:16:58 +0530

    Create BonafideApplication.java

---
+++ demo/src/main/java/com/example/demo/bonafide/BonafideApplication.java
@@ -0,0 +1,12 @@
+package com.example.bonafide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonafideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BonafideApplication.class, args);
+    }
+}
